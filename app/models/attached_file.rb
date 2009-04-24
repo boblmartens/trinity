64e683198c4a65f9a53b4515a19534c32d181b6c
Bob Martens <boblmartens@gmail.com>
2009-04-23 20:36:41 -0500

    update for last commit, forgot one test

--- app/models/attached_file.rb
+++ app/models/attached_file.rb
@@ -1,5 +1,6 @@
 class AttachedFile < ActiveRecord::Base
   belongs_to :service
+  has_many :comments, :as => :commentable
 
   validates_presence_of :name, :description, :user_id
 end
--- test/unit/attached_file_test.rb
+++ test/unit/attached_file_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class AttachedFileTest < ActiveSupport::TestCase
   should_belong_to :service
+  should_have_many :comments
 
   should_validate_presence_of :name, :description, :user_id
 end
