f3191277eb4fb8b7417aa03b93cdc1278061a563
Bob Martens <boblmartens@gmail.com>
2009-05-03 19:15:34 -0500

    modified test suite; all tests green

--- app/models/attached_file.rb
+++ app/models/attached_file.rb
@@ -2,7 +2,7 @@ class AttachedFile < ActiveRecord::Base
   belongs_to :service
   has_many :comments, :as => :commentable
 
-# validates_presence_of :name, :description, :user_id
+  validates_presence_of :name, :description, :user_id
   
   has_attached_file :attached
 end
--- test/unit/service_test.rb
+++ test/unit/service_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
 class ServiceTest < ActiveSupport::TestCase
-  should_validate_presence_of :name, :season, :description, :other
+  should_validate_presence_of :name, :season_id, :description
   should_have_many :service_times, :attached_files, :comments
 end
